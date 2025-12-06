574aa39eb931332dfedb95c2b0864493f99c1d30
Melinda Belabbas <belabbasmelinda@gmail.com>
2025-12-07 00:14:37 +0100

    ajout des algo des graph + les graph

---
+++ TP2/graph.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv("PARTIE2.csv")   
+
+
+df_A = df[df["Algo"] == "A"]
+df_B = df[df["Algo"] == "B"]
+
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(
+    df_A["N"], df_A["Temps"],
+    color="green",
+    marker="s",          
+    markersize=8,
+    label="Fonction MaxEtMinA"
+)
+
+plt.plot(
+    df_B["N"], df_B["Temps"],
+    color="blue",
+    marker="s",          
+    markersize=8,
+    label="Fonction MaxEtMinB"
+)
+plt.xticks(sorted(df["N"].unique()))
+plt.ticklabel_format(style='plain', axis='x')
+
+plt.xlabel("N (Taille du tableau)", fontweight="bold", labelpad=10) 
+plt.ylabel("Temps (s)", fontweight="bold", labelpad=10) 
+
+plt.title("Comparaison entre MaxEtMinA et MaxEtMinB",fontweight="bold")
+plt.grid(True)
+plt.legend()            
+
+plt.show()
---
+++ TP2/histo.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+df = pd.read_csv("PARTIE2.csv")
+
+
+df_A = df[df["Algo"] == "A"]
+df_B = df[df["Algo"] == "B"]
+
+
+x = np.arange(len(df_A["N"])) 
+width = 0.35 
+
+plt.figure(figsize=(10, 6))
+
+plt.bar(x - width/2, df_A["Compteur"], width, label="Fonction MaxEtMinA", color="green")
+plt.bar(x + width/2, df_B["Compteur"], width, label="Fonction MaxEtMinB", color="blue")
+
+
+plt.xticks(x, df_A["N"], rotation=0)  
+
+plt.ticklabel_format(style='plain', axis='y')
+
+plt.xlabel("N (Taille du tableau)", fontweight="bold", labelpad=10)
+plt.ylabel("Nombre de comparaisons", fontweight="bold", labelpad=10)
+plt.title("Comparaison du nombre de comparaisons", fontweight="bold")
+plt.legend()
+plt.grid(axis="y")
+
+plt.show()
