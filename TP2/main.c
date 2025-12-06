6c2a794d6ecaba6ae6a44f34a971fd8cb3166a6c
Melinda Belabbas <belabbasmelinda@gmail.com>
2025-12-06 23:10:27 +0100

    main partie 2 avec csv

--- TP2/main.c
+++ TP2/main.c
@@ -1,28 +1,92 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <intrin.h>
+#include <stdint.h>
+#include <windows.h>
+#include <stdbool.h>
 
-// algo2
-int rechercheElmntSequent(int tab[], int n, int x) {
-    for (int i = 0; i < n; i++) {
-        if (tab[i] == x) return 1; // trouvé
-    }
-    return 0; // pas trouvé
+// structure personnalisee pour le temps haute précision sous windows
+typedef struct {
+    long tv_sec;
+    long tv_nsec;
+} high_res_time_t;
+
+// fonctions de timing haute précision pour windows
+void get_high_precision_time(high_res_time_t *ts) {
+    LARGE_INTEGER frequency, counter;
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&counter);
+    ts->tv_sec = (long)(counter.QuadPart / frequency.QuadPart);
+    ts->tv_nsec = (long)(((counter.QuadPart % frequency.QuadPart) * 1000000000LL) / frequency.QuadPart);
 }
 
-// algo5
-void MaxEtMinB(int tab[], int n, int *max, int *min, long *compteur) {
-    *compteur = 0;
-    //si la taille est pair on compare les deux premeirs pour init le max et min sinon on prend que le 1er elmnt
+double get_time_diff_s(high_res_time_t *start, high_res_time_t *end) {
+    return (end->tv_sec - start->tv_sec) + (end->tv_nsec - start->tv_nsec) / 1e9;
+}
+
+
+void genereTableauSansRepetition(long long tab[], long long n) {
+    if (n <= 0) return;
+    int bit;
+    for (long long i = 0; i < n; i++) {
+        tab[i] = i;
+    }
+    for (int i = n - 1; i > 0; i--) {
+        long long  j = (long)rand() % (i + 1);
+        long long temp = tab[i];
+        if ((bit = (rand() > RAND_MAX / 2))==1) {
+            tab[i] = -1*tab[j];
+        }
+        tab[j] = temp;
+    }
+}
+
+double MaxEtMinA(long long tab[], long long n, long long * Max, long long * Min, long long * CompteurA) { //retourne le temps
+    double time_taken;
+    high_res_time_t start, end;
+    *CompteurA=0;
+    get_high_precision_time(&start);
+
+    *Max = tab[0];
+    *Min = tab[0];
+    for (int i = 1; i < n; i++) {
+        (*CompteurA) ++;
+        if (*Min > tab[i]) {
+            *Min = tab[i];
+        }
+        (*CompteurA) ++;
+        if (*Max < tab[i]) {
+            *Max = tab[i];
+        }
+    }
+
+    get_high_precision_time(&end);
+    time_taken = get_time_diff_s(&start, &end);
+
+    return time_taken;
+}
+
+double MaxEtMinB(long long tab[], long long n, long long *Max, long long *Min, long long *CompteurB) {
+    double time_taken;
+    high_res_time_t start, end;
+    *CompteurB = 0;
+
+    get_high_precision_time(&start);
+    //si la taille est paire on compare les deux premiers pour init le max et min sinon on prend que le 1er elmnt
     if (n % 2 == 0) {
-        *max = tab[0] > tab[1] ? tab[0] : tab[1];
-        *min = tab[0] < tab[1] ? tab[0] : tab[1];
+        *Max = tab[0] > tab[1] ? tab[0] : tab[1];
+        *Min = tab[0] < tab[1] ? tab[0] : tab[1];
     } else {
-        *max = *min = tab[0];
+        *Max = *Min = tab[0];
     }
 
     for (int i = 0; i < n-1; i += 2) {
-        (*compteur)++;
+        (*CompteurB)++;
         int a = tab[i];
         int b = tab[i+1];
         if (a > b) {
@@ -33,113 +97,50 @@ void MaxEtMinB(int tab[], int n, int *max, int *min, long *compteur) {
             tab[i+1] = b;
         }
 
-        (*compteur)++;
-        if (tab[i] < *min) *min = tab[i];
-        (*compteur)++;
-        if (tab[i+1] > *max) *max = tab[i+1];
+        (*CompteurB)++;
+        if (tab[i] < *Min) *Min = tab[i];
+        (*CompteurB)++;
+        if (tab[i+1] > *Max) *Max = tab[i+1];
     }
 
     if (n % 2 != 0) {
-        if (tab[n-1] < *min) *min = tab[n-1];
-        if (tab[n-1] > *max) *max = tab[n-1];
+        if (tab[n-1] < *Min) *Min = tab[n-1];
+        if (tab[n-1] > *Max) *Max = tab[n-1];
     }
+    get_high_precision_time(&end);
+    time_taken = get_time_diff_s(&start, &end);
+    return time_taken;
 }
 
-// Fonction pour générer un tableau trié (pour les algo 2 et 3)
-void genereTableauTrieAvecRepetition(int tab[], int n) {
-    for (int i = 0; i < n; i++) {
-        tab[i] = rand() % 1000001;
-    }
-    // tri simple
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-1-i; j++) {
-            if (tab[j] > tab[j+1]) {
-                int tmp = tab[j];
-                tab[j] = tab[j+1];
-                tab[j+1] = tmp;
-            }
-        }
-    }
-}
-
-// Fonction pour générer un tableau sans répétition (pour algo4 et 5)
-void genereTableauSansRepetition(int tab[], int n) {
-    int *existe = calloc(1000001, sizeof(int));
-    int count = 0;
-    while (count < n) {
-        int val = rand() % 1000001;
-        if (!existe[val]) {
-            tab[count++] = val;
-            existe[val] = 1;
-        }
-    }
-    free(existe);
-}
 
 int main() {
-    srand(time(NULL));
+    FILE *f = fopen("PARTIE2.csv", "w");
+    fprintf(f, "Algo,N,Temps,Compteur\n");
 
-    printf("Recherche dans un tableau trié (valeurs repetées possibles)\n");
-    int n2;
-    printf("Entrez la taille du tableau pour algo2 : ");
-    scanf("%d", &n2);
+    double temps1,temps2;
+    int t,i=0;
+    long long valeurs_n[10] = {100000, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000, 1600000, 1800000};
 
-    int *tab2 = malloc(n2 * sizeof(int));
-    if (!tab2) return 1;
+    while (i<10) {
+        long long *tab = malloc(valeurs_n[i] * sizeof(long long));
+        if (!tab) return 1;
 
-    genereTableauTrieAvecRepetition(tab2, n2);
+        genereTableauSansRepetition(tab, valeurs_n[i]);
 
-    printf("Tableau trie : ");
-    for (int i = 0; i < n2; i++) printf("%d ", tab2[i]);
-    printf("\n");
+        long long Max,Min,Max2,Min2;
+        long long CompteurA,CompteurB;
 
-    int x;
-    printf("Entrez la valeur a rechercher : ");
-    scanf("%d", &x);
-    
-    //test algo2
-    clock_t debut = clock();
-    int res = rechercheElmntSequent(tab2, n2, x);
-    clock_t fin = clock();
+        temps1=MaxEtMinA(tab,valeurs_n[i],&Max,&Min,&CompteurA);
+        fprintf(f,"A,%lld,%f,%lld\n",valeurs_n[i],temps1,CompteurA);
+        temps2=MaxEtMinB(tab,valeurs_n[i],&Max2,&Min2,&CompteurB);
+        fprintf(f,"B,%lld,%f,%lld\n",valeurs_n[i],temps2,CompteurB);
 
-    double temps = ((double)(fin - debut)) / CLOCKS_PER_SEC;
-
-    if (res == 1)
-        printf("La valeur %d existe dans le tableau.\n", x);
-    else
-        printf("La valeur %d n'existe pas dans le tableau.\n", x);
-    printf("Temps d'exécution : %.6f s\n", temps);
-
-    free(tab2);
-
-    //test algo5
-    printf("\nRecherche Max et Min par paires\n");
-    int n5;
-    printf("Entrez la taille du tableau a generer : ");
-    scanf("%d", &n5);
-
-    int *tab5 = malloc(n5 * sizeof(int));
-    if (!tab5) return 1;
-
-    genereTableauSansRepetition(tab5, n5);
-
-    printf("Tableau original : ");
-    for (int i = 0; i < n5; i++) printf("%d ", tab5[i]);
-    printf("\n");
-
-    int max, min;
-    long compteur;
-    debut = clock();
-    MaxEtMinB(tab5, n5, &max, &min, &compteur);
-    fin = clock();
-    temps = ((double)(fin - debut)) / CLOCKS_PER_SEC;
-
-    printf("Tableau apres organisation par paires : ");
-    for (int i = 0; i < n5; i++) printf("%d ", tab5[i]);
-    printf("\n");
-
-    printf("Max: %d, Min: %d, Comparaisons: %ld, Temps: %.6f s\n", max, min, compteur, temps);
-
-    free(tab5);
+        printf("\nTaille du tableau: %lld, Max: %lld, Min: %lld\nTemps MaxEtMinA: %f, Nombres de comparaisons: %lld\nTemps MaxEtMinB: %f, Nombres de comparaisons: %lld\n",valeurs_n[i], Max,Min,temps1,CompteurA,temps2,CompteurB);
+        free(tab);
+        i++;
+    }
+    fclose(f);
     return 0;
+
+
 }
